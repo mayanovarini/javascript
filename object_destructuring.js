245a0954333e9dbd593f120c442a0950007ecc50
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-04 23:09:39 -0700

    object destructuring

---
+++ object_destructuring.js
@@ -0,0 +1,13 @@
+// split an object into variables by destructuring the object
+
+let options = {
+  title: "Menu",
+  width: 100,
+  height: 200
+};
+
+let {title, width, height} = options;
+
+alert(title);  // Menu
+alert(width);  // 100
+alert(height); // 200
