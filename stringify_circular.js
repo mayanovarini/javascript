e22c870b82a5a2083ccea4d26fc098d0fd8f1268
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-05 02:40:19 -0700

    circular references in stringify

---
+++ stringify_circular.js
@@ -0,0 +1,34 @@
+// avoiding circular references for JSON Stringify 
+
+// the following will result in error due to circular references between variables
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: ["john", "ann"]
+};
+
+meetup.place = room;       // meetup references room
+room.occupiedBy = meetup; // room references meetup
+
+JSON.stringify(meetup); // Error: Converting circular structure to JSON
+
+// the correct way to get this to work : 
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: [{name: "John"}, {name: "Alice"}],
+  place: room // meetup references room
+};
+
+room.occupiedBy = meetup; // room references meetup
+
+alert( JSON.stringify(meetup, ['title', 'participants']) );
+// {"title":"Conference","participants":[{},{}]}
