42064aa6f16bbaff08826af88ef65f57e997ad25
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-04 23:42:55 -0700

    nested destructuring

--- object_destructuring.js
+++ object_destructuring.js
@@ -11,3 +11,30 @@ let {title, width, height} = options;
 alert(title);  // Menu
 alert(width);  // 100
 alert(height); // 200
+
+// Nested destructuring 
+
+let options = {
+  size: {
+    width: 100,
+    height: 200
+  },
+  items: ["Cake", "Donut"],
+  extra: true    // something extra that we will not destruct
+};
+
+// destructuring assignment on multiple lines for clarity
+let {
+  size: { // put size here
+    width,
+    height
+  },
+  items: [item1, item2], // assign items here
+  title = "Menu" // not present in the object (default value is used)
+} = options;
+
+alert(title);  // Menu
+alert(width);  // 100
+alert(height); // 200
+alert(item1);  // Cake
+alert(item2);  // Donut
