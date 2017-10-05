d115d3a8803f9fa108a94fbab2c8a31c103277a5
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-04 20:51:23 -0700

    anagram in js with map

---
+++ anagram.js
@@ -0,0 +1,24 @@
+/* anagram */
+
+function aclean(arr) {
+  let map = new Map();
+
+  for(let word of arr) {
+    // split the word by letters, sort them and join back
+    let sorted = word.toLowerCase().split('').sort().join(''); // (*)
+
+    /* 
+    If we ever meet a word the same letter-sorted form again, 
+    then it would overwrite the previous value with the same key in the map. 
+    So we’ll always have at maximum one word per letter-form.*/
+
+    map.set(sorted, word); #
+  }
+
+  // takes an iterable over map values (we don’t need keys in the result) and returns an array of them.
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) );
