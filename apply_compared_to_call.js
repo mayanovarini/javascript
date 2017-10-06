4e0827f0b6172c22b7f5c3bc3ca406ba60cfbf22
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-06 13:24:27 -0700

    apply spreads an array argument

--- apply_compared_to_call.js
+++ apply_compared_to_call.js
@@ -15,7 +15,20 @@ sumThem.call(calcOne, 1,2,3,4);
 sumThem.apply(calcTwo, [1,2,3,4]);
 
 // the difference is apparent when a function that call/apply gets called on using parameters
-// apply will need the parameters to be just two, the object and an array of the required parameters
+// apply will need the parameters to be just two, the object and an array of 
+// the required parameters
 // call will take them as comma separated values
 
-// use apply when the function DOES NOT take an array as an argument, apply will then spread the array for you
+// use apply when the function DOES NOT take an array as an argument, 
+// apply will then spread the array for you - see example below :
+
+function sumValues(a,b,c) {
+	return a+b+c;
+}
+
+var values = [1,3,5]; 
+
+sumValues(values); // "1,3,5undefinedundefined"
+
+sumValues.apply(this, values); // 9 
+
