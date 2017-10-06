77c4663bfc9f0b34f28ffd774af5535ddcd97786
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-05 21:51:08 -0700

    avoid callback hell

--- script_loading_callback.js
+++ script_loading_callback.js
@@ -20,3 +20,54 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', s
   alert(`Cool, the ${script.src} is loaded`);
   alert( _ ); // function declared in the loaded script
 });
+
+// with error handling 
+
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src}`));
+
+  document.head.append(script);
+}
+
+// error-first callback
+loadScript('/my/script.js', function(error, script) {
+  if (error) {
+    // handle error
+  } else {
+    // script loaded successfully
+  }
+});
+
+// ways to avoid callback hell for multiple callbacks
+
+loadScript('1.js', step1);
+
+function step1(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('2.js', step2);
+  }
+}
+
+function step2(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('3.js', step3);
+  }
+}
+
+function step3(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...continue after all scripts are loaded (*)
+  }
+};
