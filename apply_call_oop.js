1377bdc7056bf7f6b8ccfd50a152c128db6bc84a
thegirlfromipanema <mayanovarini@gmail.com>
2017-10-07 19:11:19 -0700

    arguments keyword

--- apply_call_oop.js
+++ apply_call_oop.js
@@ -18,3 +18,10 @@ function Motorcycle(make, model, year){
 	Car.apply(this, [make, model, year]); // apply only takes 2 arguments, 'this' and an array
 	this.numWheels = 2;
 }
+
+function Motorcycle(make, model, year){
+	// arguments is a special keyword
+	// arguments is an array-like object storing all of the arguments that will be pased into the function
+	car.apply(this, arguments); 
+	this.numWheels = 2;
+}
