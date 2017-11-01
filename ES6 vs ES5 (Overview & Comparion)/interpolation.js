c7fd41e9a3ff8beec4d97335e47b51a335de9bd5
thegirlfromipanema <mayanovarini@gmail.com>
2017-11-01 13:48:46 -0700

    string interpolation

---
+++ ES6 vs ES5 (Overview & Comparion)/interpolation.js
@@ -0,0 +1,13 @@
+// String Interpolation
+
+// ES6
+
+let dog = { name: "Noodles" }
+
+let message = "Hello ${dog.name}" // Hello Noodles
+
+// ES5
+
+var dog = { name: "Noodles"}
+
+var message = "Hello " + dog.name; // Hello Noodles
