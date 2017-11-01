b54693aedcbffcd42afe943ca3028430dfb2501b
thegirlfromipanema <mayanovarini@gmail.com>
2017-11-01 14:14:51 -0700

    method properties with method notation

--- ES6 vs ES5 (Overview & Comparion)/object_prop.js
+++ ES6 vs ES5 (Overview & Comparion)/object_prop.js
@@ -23,3 +23,33 @@ var obj = {
     studentId: 42,
 };
 obj[ "First Name" + lastName() ]: "Exists"; 
+
+// Method properties
+
+// ES6 - Support for method notation in object property definitions, for both regular functions and generator functions.
+
+
+obj = {
+    foo (a, b) {
+        …
+    },
+    bar (x, y) {
+        …
+    },
+    *doThat (x, y) { // method notation with asterisk
+        …
+    }
+}
+
+// ES5
+
+obj = {
+    foo: function (a, b) {
+        …
+    },
+    bar: function (x, y) {
+        …
+    },
+    //  sayThat: no equivalent in ES5
+    …
+};
