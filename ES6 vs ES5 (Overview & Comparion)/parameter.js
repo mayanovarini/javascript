3ff0c053ff2245ecde5fb34d485002eb5ead5561
thegirlfromipanema <mayanovarini@gmail.com>
2017-11-01 13:44:50 -0700

    rest parameters

--- ES6 vs ES5 (Overview & Comparion)/parameter.js
+++ ES6 vs ES5 (Overview & Comparion)/parameter.js
@@ -1,3 +1,5 @@
+// Default Parameters
+
 // ES6 allows easy setup for default valued parameters
 
 function a (x, y = "bar") {
@@ -16,3 +18,19 @@ function a (x, y) {
 };
 
 a("foo") // "foo bar"
+
+// Rest Parameters
+
+// ES6 
+function a (x, y, ...b) {
+	return (x + y) + b.length
+}
+
+a(4, 2, "hello", "world"); // 8
+
+// ES5
+function a (x, y) {
+	var b = Array.prototype.slice.call(arguments, 2);
+};
+
+a(4, 2, "hello", "world") // 8
