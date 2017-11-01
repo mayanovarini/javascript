51467c37d6f697311a5d6cd1951a5e6be75a9bb7
thegirlfromipanema <mayanovarini@gmail.com>
2017-11-01 14:10:58 -0700

    computer property names

--- ES6 vs ES5 (Overview & Comparion)/object_prop.js
+++ ES6 vs ES5 (Overview & Comparion)/object_prop.js
@@ -1,3 +1,5 @@
+// Syntax discounts
+
 // ES6
 
 obj = { x, y }
@@ -5,3 +7,19 @@ obj = { x, y }
 // ES5
 
 obj = { x: x, y: y };
+
+// Computed property names
+
+// ES6 can do it inside the object literal declaration
+
+let obj = {
+    studentId: 42,
+    [ "First Name" + lastName() ]: "Exists"
+}
+
+// ES5 has to be outside the object declaration
+
+var obj = {
+    studentId: 42,
+};
+obj[ "First Name" + lastName() ]: "Exists"; 
