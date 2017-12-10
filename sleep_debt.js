e15b9e8e049b0d17dda7d5be5b91b6ad4ea0c128
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-10 07:09:22 -0800

    updated shorter syntax

--- sleep_debt.js
+++ sleep_debt.js
@@ -1,26 +1,33 @@
 const getSleepHours = day => {
-  switch(day) {
-    case "Monday":
-      return 8;
-    case "Tuesday":
-      return 5;
-    case "Wednesday":
-      return 8;
-    case "Thursday":
-      return 6;
-    case "Friday":
-      return 8;
-    case "Saturday":
-      return 5;
-    case "Sunday":
-      return 7;
-  }
-}
-
-const getActualSleepHours = () => {
-  return getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + 
-  getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + 
-  getSleepHours('Sunday');
+  let hours = prompt('how many hours of sleep did you get on', day);
+  return +hours;
 };
 
-getActualSleepHours()
+const getActualSleepHours = () => {
+  return (
+    getSleepHours('Monday') +
+    getSleepHours('Tuesday') +
+    getSleepHours('Wednesday') +
+    getSleepHours('Thursday') +
+    getSleepHours('Friday') +
+    getSleepHours('Saturday') +
+    getSleepHours('Sunday')
+  );
+};
+
+const getIdealSleepHours = () => 7 * 7.54;
+
+const calculateSleepDebt = () => {
+  let actualSleepHours = getActualSleepHours();
+  let ideaSleepHours = getIdealSleepHours();
+
+  if (actualSleepHours === ideaSleepHours) {
+    console.log('nice sleep!');
+  }
+
+  actualSleepHours > ideaSleepHours ?
+    console.log('too much sleep', actualSleepHours, 'hrs') :
+    console.log('not enough sleep!', actualSleepHours, 'hrs');
+  }
+
+calculateSleepDebt();
