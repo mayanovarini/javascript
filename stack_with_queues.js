6b7576ad8069158d59f3a7e1c678cb6e6d4e8a4e
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-18 21:43:13 -0800

    solution #2 for stack using queues

--- stack_with_queues.js
+++ stack_with_queues.js
@@ -1,4 +1,5 @@
 /*
+Solution #1
 Runtime Complexity
 push(): Constant, O(1).
 
@@ -43,5 +44,49 @@ class stack_using_queue {
     this.swap_queues();
 
     return value;
+  }
+}
+
+/*
+Solution #2
+Runtime Complexity
+push(): Linear, O(n).
+
+pop(): Constant, O(1).
+
+Memory Complexity
+Linear, O(n).
+*/
+
+class stack_using_queue_2 {
+  constructor() {
+    this.queue1 = [];
+    this.queue2 = [];
+  }
+  swap_queues() {
+    let temp = this.queue1;
+    this.queue1 = this.queue2;
+    this.queue2 = temp;
+  }
+
+  push(data) {
+    if (this.queue1.length === 0) {
+      this.queue1.push(data);
+    } else {
+      this.queue2.push(data);
+      while (this.queue1.length !== 0) {
+        this.queue2.push(this.queue1.shift());
+      }
+      this.swap_queues();
+    }
+  }
+  isEmpty() {
+    return this.queue1.length + this.queue2.length === 0;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      throw "stack is empty";
+    }
+    return this.queue1.shift();
   }
 }
