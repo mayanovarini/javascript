77f7248b6cd4a616c9fed66460f824ecbe5f6fc5
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-19 22:50:49 -0800

    Find missing number

---
+++ find_missing_number.js
@@ -0,0 +1,27 @@
+/*
+Given an unsorted array of positive numbers from 1 to n,
+such that all numbers from 1 to n are present except one number 'x'.
+
+Objective : Find 'x'.
+
+Solution :
+Runtime Complexity
+Linear, O(n).
+
+Memory Complexity
+Constant, O(1).
+*/
+
+let find_missing = function(input) {
+  //  calculate sum of all integers
+  //  in input list
+
+  let actual_sum = 0;
+  for (let i in input) {
+    actual_sum += input[i];
+  }
+  //  There is exactly 1 number missing
+  let n = input.length + 1;
+  let sum_of_n = Math.floor((n * (n + 1)) / 2);
+  return sum_of_n - actual_sum;
+};
