f746e2b1911890f97780483783f86e33d9709aaf
thegirlfromipanema <mayanovarini@gmail.com>
2018-02-01 23:15:49 -0800

    add a non prompting functino for rps game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,9 @@
 const getUserChoice = userInput => {
-  if (userInput.toLowerCase() == 'rock' || userInput == 'paper' || userInput == 'scissors') {
+  if (
+    userInput.toLowerCase() == 'rock' ||
+    userInput == 'paper' ||
+    userInput == 'scissors'
+  ) {
     return userInput;
   } else {
     console.log('Please choose rock, paper, or scissors.');
@@ -19,34 +23,47 @@ const getComputerChoice = () => {
 
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    return 'Tie, well, that\'s cool!';
+    return "Tie, well, that's cool!";
   }
 
   if (userChoice === 'rock' && computerChoice === 'scissors') {
     return 'You win!';
-  } else {
-    return 'You lose! OMG';
   }
 
   if (userChoice === 'paper' && computerChoice === 'rock') {
-    return 'You win! Hooray!!';
-  } else {
-    return 'You lose! OMG';
+    return 'You win!';
   }
 
-  if (userChoice === 'scissors' && computerChoice === 'rock') {
-    return 'You win! Hooray!!';
-  } else {
-    return 'You lose! OMG';
+  if (userChoice === 'scissors' && computerChoice === 'paper') {
+    return 'You win!';
   }
+
+  return 'You lose! OMG';
 };
 
 const playGame = () => {
-  const userChoice = getUserChoice('scissors');
+  let userChoiceInput = prompt('Scissors or Rock or Paper');
+  const userChoice = getUserChoice(userChoiceInput);
   const computerChoice = getComputerChoice();
   console.log('You threw: ' + userChoice);
   console.log('The computer threw: ' + computerChoice);
   console.log(determineWinner(userChoice, computerChoice));
 };
 
-playGame();
+
+const playGame2 = () => {
+  let userChoices = ['scissors', 'rock', 'paper'];
+  let userChoiceInput = userChoices[Math.floor(Math.random() * 3)];
+
+  const userChoice = getUserChoice(userChoiceInput);
+  const computerChoice = getComputerChoice();
+  console.log('You threw: ' + userChoice);
+  console.log('The computer threw: ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+// uncomment to run playGame() function that will prompt a question for the user
+// playGame();
+
+// uncomment and run the function playGame2 below to run non-prompting code
+playGame2();
